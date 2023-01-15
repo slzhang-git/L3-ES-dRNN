6edea1dccf5b951f558f9c670337e0df13f90f8d
slzhang-git <92975752+slzhang-git@users.noreply.github.com>
2023-01-15 18:19:40 +0100

    Update result_visualization.py

--- output/23/result_visualization.py
+++ output/23/result_visualization.py
@@ -6,27 +6,15 @@ country_index = 1
 
 ground_truth_path = os.path.realpath(os.getcwd() + '/../../data/' + 'MHL_test.csv')
 ground_truth_path_country = os.path.realpath(os.getcwd() + '/../../data/' + 'MHL_name.csv')
-# print(ground_truth_path)
 ground_tr = pd.read_csv(ground_truth_path, header=None)
 ground_tr_value = ground_tr[country_index].values
-# print(ground_tr[28].values)
-# print(len(ground_tr_value))
 
 ground_tr_name = pd.read_csv(ground_truth_path_country, header=None)
 ground_tr_name_value = ground_tr_name[country_index].values
 
-print(ground_tr_name_value)
-print(type(ground_tr_name_value))
 
 DATA_PATH = 'w1e5.csv'
 testDates_df = pd.read_csv(DATA_PATH, header=None)
-# testDates_df.columns
-# testDates_df.iloc[:,0]
-# testDates=pd.to_datetime(testDates_df.iloc[:,0])
-# print(testDates_df.values[2])
-
-# print(range(testDates_df.values))
-
 
 load_series = list()
 for record in testDates_df.values:
@@ -35,15 +23,6 @@ for record in testDates_df.values:
         while index < len(record):
             load_series.append(float(record[index]))
             index = index + 4
-        # print(type(record))
-        # print(len(record))
-        # print(record[0:2])
-        # print(record[3:3:-1])
-        # print(record)
-        # load_series=load_series.append(record[3:3:-1])
-        # print('22222222')
-# print(load_series)
-# print(len(load_series))
 
 plt.plot(ground_tr_value, linestyle='solid', linewidth=1)  
 plt.plot(load_series, linestyle='dotted', linewidth=1)  
